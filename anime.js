a982cf2013354c7839e45b43c801ce85758d5928
StasIpatov <166314021+StasIpatov@users.noreply.github.com>
2024-09-09 10:14:27 +0300

    Add files via upload

--- anime.js
+++ anime.js
@@ -1,4 +1,25 @@
 anime({
-    targets: '.fiximg',       
+  targets: '.fiximg',       
     rotate: '3turn'
   });
+
+  let logo1 = anime({
+    targets: '.fiximg',
+    keyframes: [
+     
+
+      {
+        rotate: '1turn',
+        scale: 1.2,
+      },
+      {
+        rotate: '2turn',
+        scale: 1,
+      }
+    ],
+    duration: 2000,
+    // easing: "linear",
+    autoplay: false,
+  
+  })
+  document.querySelector('.fiximg') .addEventListener('click', logo1.play)
